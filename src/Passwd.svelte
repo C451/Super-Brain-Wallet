1221b408e123e2eadf8cda7e702a2b62f6192e88
C451 <codeallright@protonmail.com>
2022-05-28 16:31:07 +0400

    Fix layout

--- src/Passwd.svelte
+++ src/Passwd.svelte
@@ -17,25 +17,33 @@
 <style>
 .passwd-input {
     background: #0006;
-    width: 411px;
+    width: 412px;
     height: 25px;
     border: 2px solid #808080;
     /*border-radius: 5px;*/
     color: white;
     font-family: Pixel1;
     padding-left: 5px;
-    margin: 3px 0 0 0px
+    /*margin: 3px 0 0 0px;*/
+    float: left;
+}
+.passwd-wrap {
+    margin: 3px 0 0 2px;
 }
 .passwd-input:focus {
     outline:none;
 }
 .passwd-visable {
     margin-left: -25px;
+    float: left;
+    margin-top: 8px;
 }
 </style>
-<input bind:value={pass} on:input={onUpdate}
-    placeholder="Password"
-    class="passwd-input">
-<input type=checkbox bind:checked={visable}
-    on:input={onVisable}
-    class="passwd-visable">
+<div class="passwd-wrap">
+    <input bind:value={pass} on:input={onUpdate}
+        placeholder="Password"
+        class="passwd-input">
+    <input type=checkbox bind:checked={visable}
+        on:input={onVisable}
+        class="passwd-visable">
+</div>
